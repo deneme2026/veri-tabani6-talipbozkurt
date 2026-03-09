bb118e986279bab97eac2cd2da7111e5bc13d874
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-03-09 08:33:10 +0000

    sql1

--- 1.sql
+++ 1.sql
@@ -1,3 +1,4 @@
 -- 1. 2008 filmleri
 -- Görev: 2008 yılında vizyona giren tüm filmlerin isimlerini (title) listeleyen SQL sorgusunu yazın.
 -- Sorgunuzu aşağıya yazın:
+SELECT title FROM movies WHERE year = 2008;
